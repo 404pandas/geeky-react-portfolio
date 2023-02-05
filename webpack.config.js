ebdedd93b10736a9c687832ba04c517736814bfb
404pandas <113853550+404pandas@users.noreply.github.com>
2023-02-05 09:41:04 -0500

    Set up eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1886bb8732fd4ce6c05ac25e918a13476a14b8cb683cbb5bbc8a6cbeaa9ec10
-size 2500
+oid sha256:0b7ff3cbc9be18f482adae691253a3bdd10ce2a63612d6516c373b01004eab92
+size 2399
